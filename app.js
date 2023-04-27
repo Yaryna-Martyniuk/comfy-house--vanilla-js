2e3505550bfd7fd498e7e019b96451680031d109
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-04-27 16:33:09 +0300

    chevron down action to delete item from cart enabled

--- app.js
+++ app.js
@@ -168,9 +168,12 @@ class UI {
                 if (tempItem.amount > 0) {
                     Storage.saveCart(cart);
                     this.setCartValues(cart);
-                    
+                    lowerAmount.previousElementSibling.innerText =
+                        tempItem.amount;
                 } else {
-                    cartContent.removeChild(lowerAmount.parentElement);
+                    cartContent.removeChild(
+                        lowerAmount.parentElement.parentElement
+                    );
                     this.removeItem(id);
                 }
             }
